b7dc0cf42eb7ba202c08b17dd2d3062670108569
bblakemore312 <60017095+bblakemore312@users.noreply.github.com>
2020-04-03 16:00:10 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, escape, request
 import hashlib
 import json
 
@@ -6,29 +6,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Hello, World!"
+    return "Hello There General Kenobi!"
 
 @app.route('/factorial')
 def hello():
-    factorial = request.args.get('arg')
+    num = int(input(request.args.get('arg1')))
     fact = 1
-    if factorial < 0:
+    if num < 0:
         print("Sorry, Factorial does not exist")
-    elif factorial == 0:
+    elif num == 0:
         print("The factorial of 0 is 1")
     else:
-        for i in range(1, factorial + 1):
-            fact = fact*1
-        print("The factorial of",factorial,"is",fact)
+        for i in range(1, num + 1):
+            fact = fact*i
+    print("The factorial of",num,"is",fact)
+    output1 = {
+        "input": num,
+        "output": print
+    }
 
     #return f'Hello, {escape(name)}!'
-    return 'factorial'
+    return json.dumps(output1) 
 @app.route('/md5')
 def md5_str():
     val = request.args.get("str")
     m = hashlib.md5()
     m.update(val.encode('utf8'))
-
     output = {
         "input": val,
         "output": m.hexdigest()
