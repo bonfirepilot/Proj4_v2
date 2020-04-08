22d57bfb221b74b365c1347e92166c719ec6370f
bblakemore312 <60017095+bblakemore312@users.noreply.github.com>
2020-04-08 14:31:37 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -18,23 +18,25 @@ def Fact1(n: int):
         for i in range(1, n + 1):
             fact = fact*i
         return("The factorial of",n,"is",fact)
-@app.route("/factorial")
-def send_Fact1():
-    try:
-        return str(Fact1(int(request.args['n'])))
-    except ValueError:
-        return "Please use a positive number"
- 
-@app.route('/md5')
-def md5_str():
-    val = request.args.get("str")
-    m = hashlib.md5()
-    m.update(val.encode('utf8'))
+@app.route("/factorial/<int:n>")
+def send_Fact1(n):
     output = {
-        "input": val,
+        "input": n,
+        "output": str(Fact1(n))
+    }
+    return json.dumps(output)
+ 
+@app.route("/md5/<str:v>")
+def md5_str(v):
+    # val = request.args.get("str")
+    m = hashlib.md5()
+    m.update(v.encode('utf8'))
+    output = {
+        "input": v,
         "output": m.hexdigest()
     }
     return json.dumps(output)
+
 # The Fibonacci function
 def fibo(n: int):
     """
@@ -48,31 +50,32 @@ def fibo(n: int):
         return fibo(n-1)+fibo(n-2)
 
 # The actual route
-@app.route("/fibonacci")
-def send_fibo():
+@app.route("/fibonacci/<int:n>")
+def send_fibo(n):
     """
     Sending the fibonacci number to the user, telling him if
     he uses improper input
     """
-    try:
-        return str(fibo(int(request.args['n'])))
-    except ValueError:
-        return "Please use a number as the 'n' argument"
+    output = {
+        "input": n, 
+        "output": str(fibo(n))
+    }
+    return json.dumps(output)
 def prime(n: int):
     if n > 1:
        for i in range(2, n//2):
           if(n % i) == 0:
              return(n, "is not a prime number")
-             break
        else:
          return(n, "is a prime number")
     else:
       return(n, "is not a prime number")
-@app.route("/is-prime")
-def send_prime():
-    try:
-        return str(prime(int(request.args['n'])))
-    except ValueError:
-        return "not a prime number"
+@app.route("/is-prime/<int:n>")
+def send_prime(n):
+    output = {
+        "input": n, 
+        "output": str(prime(n))
+    }
+    return json.dumps(output)
 if __name__ == '__main__':
     app.run(debug=True)
