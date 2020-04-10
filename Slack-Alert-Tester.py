d151285cafaeadc8d00304cae84e0392c2812bb0
Tristan512 <60722487+Tristan512@users.noreply.github.com>
2020-04-10 00:43:38 -0500

    Delete Slack-Alert-Tester.py

--- Slack-Alert-Tester.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-web_hook_url = 'Have the webhook saved on my computer and will put it in later'
-
-x = 6
-
-slack_msg = {'text': x }
-
-requests.post(web_hook_url,data=json.dumps(slack_msg))
