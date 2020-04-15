0ea887c5421fe3e3ad93032637ba9307003753b2
bblakemore312 <60017095+bblakemore312@users.noreply.github.com>
2020-04-15 13:46:50 -0500

    Update API.py

--- API.py
+++ API.py
@@ -49,9 +49,9 @@ def md5_str(val: str):
 def fibo_send(n: int):
     def fibo(n):
         if n < 1:
-            return False
+            return 0
         elif n == 1:
-            return False
+            return 1
         else:
             return fibo(n-1)+fibo(n-2)
     fib_list = []
