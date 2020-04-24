23c6e00cec6ca1ea3e8fc774c53efd6a16fcc47e
bblakemore312 <60017095+bblakemore312@users.noreply.github.com>
2020-04-24 14:29:54 -0500

    Update API.py

--- API.py
+++ API.py
@@ -1,11 +1,16 @@
+import os
+from math import sqrt
 from flask import Flask, escape, request, jsonify
 from urllib import request, parse
 import hashlib
+import redis
 import json
 import os
 
 
+
 app = Flask(__name__)
+redis = Redis(host="redis", socket_connect_timeout=2, socket_timeout=2)
 
 @app.route('/')
 def root():
@@ -16,7 +21,7 @@ def Fact1(n: int):
     if n < 0:
         return False
     elif n == 0:
-        return False
+        return 1
     else:
         for i in range(1, n + 1):
             fact = fact*i
@@ -25,14 +30,7 @@ def Fact1(n: int):
 def send_Fact1(n):
     output = {
         "input": n,
-        "output": str(Fact1(n))
-    }
-    return json.dumps(output)
-@app.route("/factorial/<n>")
-def not_Fact1(n):
-    output = {
-        "input": n,
-        "output": False
+        "output": int(Fact1(n))
     }
     return json.dumps(output)
 @app.route('/md5/<val>')
@@ -48,52 +46,33 @@ def md5_str(val: str):
 @app.route("/fibonacci/<int:n>")
 def fibo_send(n: int):
     def fibo(n):
-        if n < 1:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return fibo(n-1)+fibo(n-2)
-    fib_list = []
-    for iter in range(int(n)):
-        fib_list.append(fibo(iter))
-    global fib_print
-    fib_print = []
-    for k in fib_list:
-        fib_print.append(str(k))
+        a, b = 0, 1
+        array = [0]
+        while b <= n:
+            array.append(b)
+            a, b = b, a+b
+        return array
+        for i in range(0, len(array)):
+            array[i] = str(array[i])
     output = {
         "input": n, 
-        "output": fib_print
-    }
-    return json.dumps(output)
-@app.route("/fibonacci/<n>")
-def not_fibo(n):
-    output = {
-        "input": n,
-        "output": "This is not a number"
+        "output": (fibo(n))
     }
     return json.dumps(output)
 def prime(n: int):
     if n > 1:
-       for i in range(2, n//2):
-          if(n % i) == 0:
-             return False
-       else:
-         return n
-    else:
-      return False
+        for i in range(2,n):
+            if (n % i) == 0:
+                return False
+        else:
+            return True
+    if n == 1:
+        return False
 @app.route("/is-prime/<int:n>")
 def send_prime(n):
     output = {
         "input": n, 
-        "output": str(prime(n))
-    }
-    return json.dumps(output)
-@app.route("/is-prime/<n>")
-def not_prime(n):
-    output = {
-        "input": n,
-        "output": False
+        "output": (prime(n))
     }
     return json.dumps(output)
 @app.route("/slack-alert/<text>")
@@ -103,7 +82,7 @@ def send_message_to_slack(text: str):
 
     try:
         json_data = json.dumps(post)
-        req = request.Request("https://hooks.slack.com/services/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",
+        req = request.Request("https://hooks.slack.com/services/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",
                               data=json_data.encode('ascii'),
                               headers={'Content-Type': 'application/json'}) 
         resp = request.urlopen(req)
@@ -111,10 +90,40 @@ def send_message_to_slack(text: str):
         print("EXCEPTION: " + str(em))
     output = {
         "input": text,
-        "output": "sent message"
+        "output": True
     }
     return json.dumps(output)
     send_message_to_slack("<text>")
+@app.route('/keyval/<str>', methods=['GET', 'DELETE'])
+def keyval(str):
+    r = redis.Redis()
+    output = {
+        'key': str,
+        'value': None,
+        'command': 'read' if request.method=='GET' else 'DELETE',
+        'result': False,
+        'error': None
+    }
+    ##get input and check if the key exists
+    if r.exist(str) == 0:
+        output['error'] = "404 Key does not exist"
+        return jsonify(output), 404
+    
+    if request.method=='GET':
+        output['value'] = str
+        output['command'] = "Get" str
+        output['result'] = True
+        return jsonify(output), 200
+    else:
+        #delete value
+        output['key'] = None
+        output['value'] = None
+        output['command'] = "Delete" str
+        output['result'] = True
+        return jsonify(output), 200
+
+
+
 
 
 
