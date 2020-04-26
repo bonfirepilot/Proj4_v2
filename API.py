87df58376e0dbeef2f8e2e0200c25aa14b7d8eb3
bblakemore312 <60017095+bblakemore312@users.noreply.github.com>
2020-04-26 14:57:44 -0500

    Update API.py

--- API.py
+++ API.py
@@ -155,7 +155,8 @@ def key_value():
         return jsonify(_JSON), 409
 
 
-    else if request.method == 'PUT' and test_value == None:
+    else:
+        if request.method == 'PUT' and test_value == None:
         _JSON['error'] = "Cannot update record: key does not exist."
         return jsonify(_JSON), 404
 
@@ -200,7 +201,8 @@ def keyvalue_retrieve(key):
         return jsonify(_JSON), 200
 
 
-    else if response.method == 'DELETE':
+    else:
+        if response.method == 'DELETE':
         ret = redis.delete(key)
         if ret == 1:
             _JSON['result'] = True
