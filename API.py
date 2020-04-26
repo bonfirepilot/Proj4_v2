bb0da3dbecab365fcc6efb01000cec4089372603
bblakemore312 <60017095+bblakemore312@users.noreply.github.com>
2020-04-26 15:29:37 -0500

    Update API.py

--- API.py
+++ API.py
@@ -121,52 +121,52 @@ def send_message_to_slack(text: str):
    #     output['command'] = "Delete str"
     #    output['result'] = True
    #     return jsonify(output), 200
-@app.route('/keyval', methods=['POST', 'PUT'])
-def key_value():
+# @app.route('/keyval', methods=['POST', 'PUT'])
+# def key_value():
 
-    _JSON = {
-        'key': None,
-        'value': None,
-        'command': 'CREATE' if request.method=='POST' else 'UPDATE',
-        'result': False,
-        'error': None
-    }
-
-
-    try:
-        payload = request.get_json()
-        _JSON['key'] = payload['key']
-        _JSON['value'] = payload['value']
-        _JSON['command'] += f" {payload['key']}/{payload['value']}"
-    except:
-        _JSON['error'] = "Missing JSON."
-        return jsonify(_JSON), 400
+#     _JSON = {
+#         'key': None,
+#         'value': None,
+#         'command': 'CREATE' if request.method=='POST' else 'UPDATE',
+#         'result': False,
+#         'error': None
+#     }
 
 
-    try:
-        test_value = redis.get(_JSON['key'])
-    except RedisError:
-        _JSON['error'] = "Cannot connect to redis."
-        return jsonify(_JSON), 400
+#     try:
+#         payload = request.get_json()
+#         _JSON['key'] = payload['key']
+#         _JSON['value'] = payload['value']
+#         _JSON['command'] += f" {payload['key']}/{payload['value']}"
+#     except:
+#         _JSON['error'] = "Missing JSON."
+#         return jsonify(_JSON), 400
 
 
-    if request.method == 'POST' and not test_value == None:
-        _JSON['error'] = "Cannot create new record: key already exists."
-        return jsonify(_JSON), 409
+#     try:
+#         test_value = redis.get(_JSON['key'])
+#     except RedisError:
+#         _JSON['error'] = "Cannot connect to redis."
+#         return jsonify(_JSON), 400
 
 
-    elif request.method == 'PUT' and test_value == None:
-        _JSON['error'] = "Cannot update record: key does not exist."
-        return jsonify(_JSON), 404
+#     if request.method == 'POST' and not test_value == None:
+#         _JSON['error'] = "Cannot create new record: key already exists."
+#         return jsonify(_JSON), 409
 
 
-    else:
-        if redis.set(_JSON['key'], _JSON['value']) == False:
-            _JSON['error'] = "There was a problem creating the value in Redis."
-            return jsonify(_JSON), 400
-        else:
-            _JSON['result'] = True
-            return jsonify(_JSON), 200
+#     elif request.method == 'PUT' and test_value == None:
+#         _JSON['error'] = "Cannot update record: key does not exist."
+#         return jsonify(_JSON), 404
+
+
+#     else:
+#         if redis.set(_JSON['key'], _JSON['value']) == False:
+#             _JSON['error'] = "There was a problem creating the value in Redis."
+#             return jsonify(_JSON), 400
+#         else:
+#             _JSON['result'] = True
+#             return jsonify(_JSON), 200
 
 
 @app.route('/keyval/<string:key>', methods=['GET', 'DELETE'])
