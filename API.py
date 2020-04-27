2a6474b5f27136ede7a578300fe8c6ec750733d1
bblakemore312 <60017095+bblakemore312@users.noreply.github.com>
2020-04-26 22:39:29 -0500

    Update API.py

--- API.py
+++ API.py
@@ -96,94 +96,42 @@ def send_message_to_slack(text: str):
     }
     return json.dumps(output)
     send_message_to_slack("<text>")
-@app.route('/keyval', methods=['POST', 'PUT'])
-def key_value():
+@app.route('/kv-record/', methods = ["POST", "PUT"])
+def record():
+	try:
+		data = request.json
+		key, value = data.items()[0]
 
-    _JSON = {
-        'key': None,
-        'value': None,
-        'command': "CREATE" if request.method=='POST' else 'UPDATE',
-        'result': False,
-        'error': None
-    }
+		if request.method == "POST":
+			if db.exists(key):
+				return json.dumps({"input": "new-key", "output": False, "error": "Unable to add pair: key already exists."})
+			else:
+				db.set(key, value)
+				return json.dumps({"input": "new-key", "output": True})
 
+		elif request.method == "PUT":
+			if db.exists(key):
+				db.set(key, value)
+				return json.dumps({"input": "existing-key", "output": True})
+			else:
+				return json.dumps({"input": "existing-key", "output": False, "error": "Unable to update value: key does not exist."})
+				
+		else:
+			raise
+	except Exception as err:
+		return json.dumps({"output": False, "error": err})
+		
 
-    try:
-        payload = request.get_json()
-        _JSON['key'] = payload['key']
-        _JSON['value'] = payload['value']
-        _JSON['command'] += f" {payload['key']}/{payload['value']}"
-    except:
-        _JSON['error'] = "Missing JSON."
-        return jsonify(_JSON), 400
-
-
-    try:
-        test_value = redis.get(_JSON['key'])
-    except RedisError:
-        _JSON['error'] = "Cannot connect to redis."
-        return jsonify(_JSON), 400
-
-
-    if request.method == 'POST' and not test_value == None:
-        _JSON['error'] = "Cannot create new record: key already exists."
-        return jsonify(_JSON), 409
-
-
-    elif request.method == 'PUT' and test_value == None:
-        _JSON['error'] = "Cannot update record: key does not exist."
-        return jsonify(_JSON), 404
-
-
-    else:
-        if redis.set(_JSON['key'], _JSON['value']) == False:
-            _JSON['error'] = "There was a problem creating the value in Redis."
-            return jsonify(_JSON), 400
-        else:
-            _JSON['result'] = True
-            return jsonify(_JSON), 200
-
-
-@app.route('/keyval/<string:key>', methods=['GET', 'DELETE'])
-def keyvalue_retrieve(key):
-
-    _JSON = {
-        'key': key,
-        'value': None,
-        'command': "{} {}".format('RETRIEVE' if request.method=='GET' else 'DELETE', key),
-        'result': False,
-        'error': None
-    }
-
-
-    try:
-        test_value = redis.get(key)
-    except RedisError:
-        _JSON['error'] = "Cannot connect to redis."
-        return jsonify(_JSON), 400
-
-
-    if test_value == None:
-        _JSON['error'] = "Key does not exist."
-        return jsonify(_JSON), 404
-    else:
-        _JSON['value'] = test_value
-
-
-    if response.method == 'GET':
-        _JSON['result'] = True
-        return jsonify(_JSON), 200
-
-
-    elif response.method == 'DELETE':
-        ret = redis.delete(key)
-        if ret == 1:
-            _JSON['result'] = True
-            return jsonify(_JSON)
-        else:
-            _JSON['error'] = f"Unable to delete key (expected return value 1; client returned {ret})"
-            return jsonify(_JSON), 400
-
+@app.route('/kv-retrieve/<string:key>')
+def retrieve(key):
+	try:
+		if db.exists(key):
+			return json.dumps({"input": "retrieve-value", "output": db.get(key)})
+		else:
+			return json.dumps({"input": "retrieve-value", "output": False, "error": "Unable to update value: key does not exist."})
+			
+	except Exception as err:
+		return json.dumps({"output": False, "error": err})
 
 
 
